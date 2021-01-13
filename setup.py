ea67aff0720d21def804dadb6afffa8f7cef7b67
skyguidance <46770626+skyguidance@users.noreply.github.com>
2021-01-13 22:15:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="torchsummary",
-    version="1.5.1",
-    description="Model summary in PyTorch similar to `model.summary()` in Keras",
+    version="1.5.1.1",
+    description="(Advanced Pytorch-Summary)Model summary in PyTorch similar to `model.summary()` in Keras",
     url="https://github.com/sksq96/pytorch-summary",
-    author="Shubham Chandel @sksq96",
+    author="(Modified)skyguidance (Original)Shubham Chandel @sksq96",
     author_email="shubham.zeez@gmail.com",
     packages=["torchsummary"],
 )
