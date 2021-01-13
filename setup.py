f4679ce05639a1e358adc03d26e09a9f64f6fe3e
skyguidance <46770626+skyguidance@users.noreply.github.com>
2021-01-13 22:16:23 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="1.5.1.1",
     description="(Advanced Pytorch-Summary)Model summary in PyTorch similar to `model.summary()` in Keras",
     url="https://github.com/sksq96/pytorch-summary",
-    author="(Modified)skyguidance (Original)Shubham Chandel @sksq96",
+    author="(Modified)skyguidance - (Original)Shubham Chandel @sksq96",
     author_email="shubham.zeez@gmail.com",
     packages=["torchsummary"],
 )
